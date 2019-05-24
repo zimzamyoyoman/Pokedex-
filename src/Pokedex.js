a20d59a82ef7f58e951321cc8b3e66b18dd31973
Samarth Goyal <samarth_niku@yahoo.co.in>
2019-05-24 17:13:13 +0530

    Made styling changes for all components

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -4,11 +4,17 @@ import './Pokedex.css';
 
 class Pokedex extends Component {
     render() {
+        let title;
+        if (this.props.isWinner) {
+            title = <h1 className="Pokedex-winner">Winning Hand!</h1>;
+        }
+        else {
+            title = <h1 className="Pokedex-loser">Losing Hand!</h1>;
+        }
         return (
             <div className="Pokedex">
-                <h1> Pokedex! </h1>
-                <p> Total Experience: {this.props.exp} </p>
-                <p> {this.props.isWinner ? 'WINNER!' : 'LOSER!'} </p>
+                {title}
+                <h4> Total Experience: {this.props.exp} </h4>
                 <div className="Pokedex-cards">
                     {this.props.pokemon.map((p)=> (
                         <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
