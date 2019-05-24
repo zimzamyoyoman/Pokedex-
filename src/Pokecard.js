0a8962aa9f32d7e12dde0bfee8e2d637cdc279be
Samarth Goyal <samarth_niku@yahoo.co.in>
2019-05-24 14:51:17 +0530

    Added single pokecard

---
+++ src/Pokecard.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './Pokecard.css';
+const POKE_API="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+class Pokecard extends Component {
+    render() {
+        let imgSrc=`${POKE_API}${this.props.id}.png`;
+        return (
+            <div>
+                <h1> {this.props.name} </h1>
+                <img src={imgSrc}></img>
+                <div> Type: {this.props.type} </div>
+                <div> Exp: {this.props.exp} </div>
+            </div>
+        )
+    }
+}
+
+export default Pokecard; 
